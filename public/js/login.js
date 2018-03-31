e080b737130d1cb1f61b7d21c6f080a66cb9b334
MTaningco <jmigst@gmail.com>
2018-03-30 22:39:49 -0700

    InitialCommit

---
+++ public/js/login.js
@@ -0,0 +1,7 @@
+// function hello() {
+// 	console.log("hello");
+// }
+
+function register() {
+	window.location.href = './login.html'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+// var MongoClient = require('mongodb').MongoCLient;
+var url = 'mongodb://root:toor@ds157320.mlab.com:57320/cmpt218'; 
+
+var port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+var options = {
+	dotfile: 'ignore',
+	etag:false,
+	extensions: ['htm','html'],
+	index: 'html/login.html'
+}
+
+app.use('/',express.static('./public',options));
+
+
+
+
+
+http.createServer(app).listen(port);
+console.log('running on port', port);
